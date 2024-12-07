0dd769810fe1b2c7ff8fb4dda9394d8964170435
Hen Cohen <hencohen83@gmail.com>
2024-12-07 20:07:50 +0200

    add MUI Tree View component and enhance order navigation

--- dragontail-client/src/views/orders/index.tsx
+++ dragontail-client/src/views/orders/index.tsx
@@ -1,16 +1,20 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { useAppSelector, RootState } from "@/store";
 import styles from "./orders.module.scss";
 
 
 export const ViewOrders = () => {
+  console.log("ViewOrders render");
   const orders = useAppSelector((state: RootState) => state.orders.orders);
+  const navigate = useNavigate();
 
   return (
     <div className={styles.root}>
       <ul className={styles.list}>
         {orders.map((order) => (
-          <li key={order.id}>{order.name}</li>
+          <li key={order.id} onClick={()=>{
+            navigate(`/orders/${order.id}`);
+          }}>{order.name}</li>
         ))}
       </ul>
       <Outlet />
--- dragontail-client/src/views/orders/order/index.tsx
+++ dragontail-client/src/views/orders/order/index.tsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router";
+import styles from "./order.module.scss";
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
+
 
 export const ViewOrder = () => {
+  console.log("ViewOrder render");
   const { orderId } = useParams();
   return (
-    <div>
+    <div className={styles.root}>
       <h1>Order {orderId}</h1>
     </div>
   );
